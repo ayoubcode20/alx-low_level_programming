1b3abfa0a2e1d2ff13b02373177948f428dd40e1
ayoub <aoumraniayoub658@gmail.com>
2023-08-11 18:34:05 -0700

    variables_if_else_while commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: a c program that prints with c function
+ *
+ * Return: Always 0 (succes)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This my readme for the 0x01-variables_if_else_while project
