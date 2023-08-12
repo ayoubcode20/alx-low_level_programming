3763076f47e64f96171e99b048184f43ccb80ced
ayoub <aoumraniayoub658@gmail.com>
2023-08-12 04:07:06 -0700

    my hello world commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point.
+ *
+ * Descrbtion: printing a quote using write function
+ *    ssize-t write(int fd.const count);
+ *
+ * Return: 1 (failure)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.quo.59);
+	return (1);
+}
