9ee575568e25c9283cb510dc137332e33e36a2ec
ayoub <aoumraniayoub658@gmail.com>
2023-08-27 17:10:21 -0700

    The “Fizz-Buzz test”

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: FizzBuzzz game
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int count;
+
+	for (count = 1; count <= 100; count++)
+	{
+		if (count % 3 == 0 && count % 5 != 0)
+			printf("Fizz");
+		else if (count % 5 == 0 && count % 3 != 0)
+			printf("Buzz");
+		else if (count % 5 == 0 && count % 3 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", count);
+	}
+	printf(" ");
+
+	return (0);
+}
