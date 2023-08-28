e31c9d2db1c8f9063eae3184e9550a89ead2b87f
ayoub <aoumraniayoub658@gmail.com>
2023-08-28 08:33:48 -0700

    fizz buzz game

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,20 @@ int main(void)
 
 	for (count = 1; count <= 100; count++)
 	{
-		if (count % 3 == 0 && count % 5 != 0)
-			printf("Fizz ");
-		else if (count % 5 == 0 && count % 3 != 0)
-			printf("Buzz ");
+		if (count % 3 == 0 && !(num % 5 == 0))
+			printf("Fizz");
+		else if (count % 5 == 0 && !(count % 3 == 0))
+			printf("Buzz");
 		else if (count % 5 == 0 && count % 3 == 0)
 			printf("FizzBuzz ");
 		else
-			printf("%d ", count);
+			printf("%d", count);
+
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
 	}
+
 	return (0);
 }
