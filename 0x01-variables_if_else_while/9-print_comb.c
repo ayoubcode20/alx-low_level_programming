4e4da6f0d03dd16fbb871dab40386efae61411fe
ayoub <aoumraniayoub658@gmail.com>
2023-09-22 02:00:36 -0700

    commited

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,11 +14,11 @@ int main(void)
 
 	for(i = 0; i < 10; i++)
 	{
-		putchar(i + '0')
-		putchar(',')
-		putchar(' ')
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
 	}
-	putchar('\n')
+	putchar('\n');
 
 	return (0);
 }
