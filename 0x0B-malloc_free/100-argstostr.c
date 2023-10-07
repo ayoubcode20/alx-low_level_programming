e6f622bec012cfb1f085527ffddb512248ad2d10
ayoub <aoumraniayoub658@gmail.com>
2023-10-07 02:02:12 -0700

    concatenates all strings in 2d array

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates the length of a string
+ *
+ * @str: a string input
+ *
+ * Return: The length of a string
+*/
+
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+
+	return (i);
+}
+
+/**
+ * argstostr -  concatenates all the arguments of the program
+ *
+ * @ac: integer input
+ * @av: a 2d array of chars
+ *
+ * Return: a pointer to the new string
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int sum = 0, i, k = 0;
+	char *str;
+
+	if (ac <= 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		sum += _strlen(av[i]) + 1;
+
+	str = malloc((sum + 1) * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+
+	return (str);
+}
