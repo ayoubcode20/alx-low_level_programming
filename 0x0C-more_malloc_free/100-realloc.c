880696f17e815ffd11ce490b8a00ac050cc7080b
ayoub <aoumraniayoub658@gmail.com>
2023-10-09 13:21:57 -0700

    commit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,18 +17,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-	if (ptr == NULL)
-	{
-		p = malloc(new_size);
-
-		if (p == NULL)
-			return (NULL);
-		return (p);
-	}
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		return (p);
 	}
 	if (new_size > old_size)
 	{
@@ -38,7 +37,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (NULL);
 
 		for (i = 0; i < old_size && i < new_size; i++)
-			((char *)p + i) = ((char *)ptr + i);
+			*((char *)p + i) = *((char *)ptr + i);
 		free(ptr);
 	}
 
