569df0b380f2447012f305d3cf46f58b5a6193c6
ayoub <aoumraniayoub658@gmail.com>
2023-10-09 02:23:29 -0700

    commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,11 +16,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
 	ptr = malloc(nmemb * size);
+
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		ptr[i] = 0;
+
+	memset(ptr, 0, nmemb * size);
 
 	return (ptr);
 }
