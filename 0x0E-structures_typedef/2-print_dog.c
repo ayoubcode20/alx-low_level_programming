fe3f1448052dce824c5449ef25eea0cb6eeb61d5
ayoub <aoumraniayoub658@gmail.com>
2023-10-10 10:23:23 -0700

    commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,14 +15,14 @@ void print_dog(struct dog *d)
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: %s\n", "nil");
+			printf("Name: %s\n", "(nil)");
 		if (d->age != NULL)
 			printf("Age: %lf\n", d->age);
 		else
-			printf("Age: %s\n", "nil");
+			printf("Age: %s\n", "(nil)");
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: %s\n", "nil");
+			printf("Owner: %s\n", "(nil)");
 	}
 }
