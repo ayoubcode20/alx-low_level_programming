16796617331c3550598b4f42e8427aae815ab825
ayoub <aoumraniayoub658@gmail.com>
2023-10-10 09:59:58 -0700

    commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,11 +13,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *p = d;
-
-	p = malloc(sizeof(struct dog));
-
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
+	if (d != NULL)
+	{
+		p->name = name;
+		p->age = age;
+		p->owner = owner;
+	}
 }
