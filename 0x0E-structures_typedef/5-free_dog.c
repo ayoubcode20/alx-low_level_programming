9aa47bda13f2b19f94066ef0d47db9e4c9fc6337
ayoub <aoumraniayoub658@gmail.com>
2023-10-10 13:46:26 -0700

    commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * free_dog - a function that frees dogs
+ *
+ * @d: a pointer to a struct
+ *
+ * Return: nothing
+*/
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+	free(d);
+}
