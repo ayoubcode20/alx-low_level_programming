95f1add0f2185a03a6b776ee9a495f224a20285d
ayoub <aoumraniayoub658@gmail.com>
2023-10-12 06:09:03 -0700

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,12 +17,12 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_list args;
 
-	va_start(args, n);
+	va_start (args, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, int);
+		sum += va_arg (args, int);
 
-	va_end(args);
+	va_end (args);
 
 	return (sum);
 }
