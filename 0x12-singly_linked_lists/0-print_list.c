227fa87502f4df7afb598121325ef4daeb4b98c2
Ayoub <aoumraniayoub658@gmail.com>
2023-10-19 13:56:49 -0700

    commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ *
+ * @h: The first node
+ *
+ * Return: the number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t int i;
+
+	list_t *temp = h;
+	for (i = 0; temp != NULL; i++)
+	{
+		if (temp->str == NULL)
+		{
+			printf("[0] (nil)");
+			continue;
+		}
+		printf("[%lu] %s\n", temp->len, temp->str);
+		temp = temp->next;
+	}
+
+	return (i + 1);
+}
