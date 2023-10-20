bd03819745bef5c3282c85f62b373c99e74ca25c
Ayoub <aoumraniayoub658@gmail.com>
2023-10-20 07:14:58 -0700

    commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
+ * _strlen - calculates the length
+ *
+ * @s: a string
+ *
+ * Return: The length
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
  * add_node_end - adds a new node at the end of a list
  *
  * @head: a pointer to the first node
@@ -21,6 +38,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		free(new);
 		return (NULL);
 	}
+	new->len = _strlen(str);
 	new->next = NULL;
 	if (*head == NULL)
 	{
