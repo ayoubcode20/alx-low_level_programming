e1758404d8797ca15314bbfedd0ea05321e832e9
Ayoub <aoumraniayoub658@gmail.com>
2023-10-28 07:08:58 -0700

    4-clear_bit.c DONE

--- 0x14-bit_manipulation/main.c
+++ 0x14-bit_manipulation/main.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	unsigned long int n;
 
-    n = 12;
-    set_bit(&n, 1);
+    n = 1024;
+    clear_bit(&n, 10);
     printf("%lu\n", n);
     n = 0;
-    set_bit(&n, 10);
+    clear_bit(&n, 10);
     printf("%lu\n", n);
     n = 98;
-    set_bit(&n, 0);
+    clear_bit(&n, 1);
     printf("%lu\n", n);
     return (0);
 }
