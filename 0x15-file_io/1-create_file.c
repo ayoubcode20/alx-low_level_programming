03e62a5cb52b622d4ad0c1d9d7ecddee22f16794
Ayoub <aoumraniayoub658@gmail.com>
2023-10-31 09:46:52 -0700

    1-create_file.c DONE

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - calculates the length of a string
+ * @s: a string
+ *
+ * Return: the length
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s)
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
+
+/**
  * create_file - function that creates a file.
  * @filename: the file name
  * @text_content: content of the file
@@ -10,12 +29,13 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	char buff[BUFF_SIZE * 8];
+	int len = _strlen(text_content);
+	char *buff = malloc(sizeof(char) * len);
 
-	if (!filename)
+	if (!filename || !buff)
 		return (-1);
-	if (text_content)
-		buff = NULL;
+	if (!text_content)
+		free(buff)
 	else
 		buff = text_content;
 	fd = open(filename, O_WDONLY);
@@ -24,5 +44,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (buff)
-		write(fd, buff, )
+		write(fd, buff, len);
+	close(fd);
+	return (1);
 }
