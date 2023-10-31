6e242be468beb92665049c6bddb922069c16d4be
Ayoub <aoumraniayoub658@gmail.com>
2023-10-31 08:00:38 -0700

    0-read_textfile.c FIX

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,39 +10,17 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-		return (0);
-	int fd = open(filename, O_RDONLY);
+	int fd;
+	ssize_t bytes;
+	char buff[BUFF_SIZE * 8];
 
+	if (!filename || !letters)
+		return (0);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	char *str = malloc(sizeof(char) * letters);
-
-	if (!str)
-	{
-		close(fd);
-		return (0);
-	}
-	ssize_t bytes_read = read(fd, str, letters);
-
-	if (bytes_read == -1)
-	{
-		free(str);
-		close(fd);
-		return (0);
-	}
-	ssize_t bytes_wrriten = 0;
-
-	while (*str)
-		bytes_wrriten += _putchar(str++);
-	if (bytes_wrriten == -1 || bytes_wrriten != bytes_read)
-	{
-		free(str);
-		close(fd);
-		return (0);
-	}
-
-	free(str);
+	bytes = read(fd, buff, letters);
+	bytes = write(STDOUT_FILENO, buff, bytes);
 	close(fd);
-	return (bytes_wrriten);
+	return (bytes);
 }
