ceb1f9c02ca938232b90b6da5ce5d78e43ad39ba
Ayoub <aoumraniayoub658@gmail.com>
2023-11-01 13:16:01 -0700

    3-cp.c FIX

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+#define USAGE "Usage: cp file_from file_to\n"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+
 /**
  * main -  program that copies the content of a file to another file
  * @argc: number of arguments
@@ -11,40 +17,25 @@
 int main(int argc, char **argv)
 {
 	char buff[BUFF_SIZE];
-	int bytes_rd, bytes_wr, FR, FW;
+	int from_fd = 0, to_fd = 0;
+	ssize_t b;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	FR = open(argv[1], O_RDONLY);
-	FW = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	while ((bytes_rd = read(FR, buff, BUFF_SIZE)) > 0)
-	{
-		if (bytes_rd == -1)
-		{
-			close(FW);
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		bytes_wr = write(FW, buff, bytes_rd);
-		if (bytes_wr == -1 || bytes_wr != bytes_rd)
-		{
-			close(FR);
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
-	if (close(FR) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", FR);
-		exit(100);
-	}
-	if (close(FW) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", FW);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+	from_fd = open(argv[1], O_RDONLY);
+	if (from_fd == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (to_fd == -1)
+		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
+	while ((b = read(from_fd, buff, BUFF_SIZE)) > 0)
+		if (write(to_fd, buff, b) != b)
+			dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
+	if (b == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+	if (close(from_fd) == -1)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+	if (close(to_fd) == -1)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, to_fd), exit(100);
 	return (0);
 }
